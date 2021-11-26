f42b0959d82f7f84f36c504192bf84c4ee1a966f
苗木谜梦 <384970327@qq.com>
2021-11-26 15:34:34 +0800

    1
    咋瓦鲁多

--- Fox/Assets/CanavsController.cs
+++ Fox/Assets/CanavsController.cs
@@ -7,8 +7,10 @@ public class CanavsController : MonoBehaviour
     public GameObject PauseMenu;
     public void PauseGame(){
         PauseMenu.SetActive(true);
+        Time.timeScale=0f;
     }
     public void ResumeGame(){
         PauseMenu.SetActive(false);
+        Time.timeScale=1f;
     }
 }
