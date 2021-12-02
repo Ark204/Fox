7c7baa2c7386e9f6c99c081743dbd7e284387977
苗木谜梦 <384970327@qq.com>
2021-12-02 17:25:10 +0800

    1
    钥匙门

--- Fox/Assets/Scripts/leader/trigger.cs
+++ Fox/Assets/Scripts/leader/trigger.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class trigger : MonoBehaviour
 {
     private Fox Fox;
+    public GameObject Door;
+    public GameObject NoKey;
+    private bool isKey = false;
     private void Awake()
     {
         Fox = GameObject.FindGameObjectWithTag("Fox").GetComponent<Fox>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag=="Medicine" && Fox.Red < Fox.MaxRed)
+        if (other.tag == "Medicine" && Fox.Red < Fox.MaxRed)
         {
             Fox.Red += 1;
             Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "Door")
+        {
+
+            if (isKey == true)
+            {
+                Destroy(other.gameObject);
+                Door.SetActive(true);
+            }
+            else
+            {
+                NoKey.SetActive(true);
+            }
+        }
+        if (other.tag == "Key")
+        {
+            isKey = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Door")
+        {
+            NoKey.SetActive(false);
         }
     }
 }
